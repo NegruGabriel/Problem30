4412357e385bb1fe34d4ab21ee10f9da7a11ca9e
NegruGabriel <87751537+NegruGabriel@users.noreply.github.com>
2022-05-11 17:27:26 +0200

    Problema30 version almoast done

--- src/Problem30/Problem30.java
+++ src/Problem30/Problem30.java
@@ -5,44 +5,45 @@ import java.util.Scanner;
 
 public class Problem30 {
     public static void main(String[] args) {
-        boolean shouldTerminateTheProgram =true;
-        boolean canContinueRunProgram =false;
+        boolean shouldTerminateTheProgram = true;
+        boolean canContinueRunProgram = false;
 
         System.out.println("Hello user. Welcome to the program");
 
-        ArrayList<String>nameEmployees =new ArrayList<>();
-        nameEmployees.add("Florin Neagu");
-        nameEmployees.add("Diana Albu");
-        nameEmployees.add("Elena Silvan");
-        nameEmployees.add("Mirel Condur");
-
-        ArrayList<Double>salaryEmployees = new ArrayList<>();
-        salaryEmployees.add(99999.99);
-        salaryEmployees.add(10000000.05);
-        salaryEmployees.add(9777546.00);
-        salaryEmployees.add(7896.65);
-
-        for (int n =0; n<nameEmployees.size(); n++){
-            System.out.println(nameEmployees.get(n) + " - " +salaryEmployees.get(n));
-        }
-
-        System.out.println("Tha salary of the employees is : "+salaryEmployees);
+        // names = []
+        ArrayList<String> nameEmployees = new ArrayList<>();
+        //
+        ArrayList<Double> salaryEmployees = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
+        String userAnswer;
 
-        System.out.println("Please enter the Employee name: ");
-        String employeesName= sc.nextLine();
+        // read name
+        System.out.print("Please enter the Employee name: ");
+        String employeesName = sc.nextLine();
 
-        System.out.println("Please enter the Employee salary: ");
-        Double employeesSalary = sc.nextDouble();
+        do {
+            //read salary
+            System.out.print("Please enter the Employee salary: ");
+            Double employeesSalary = sc.nextDouble();
+            sc.nextLine();
+
+            // add to lists
+            nameEmployees.add(employeesName);
+            salaryEmployees.add(employeesSalary);
+
+            // read name
+            System.out.print("Please enter the Employee name: ");
+            employeesName = sc.nextLine();
+        }
+        while (!employeesName.equals("quit"));
+
+        for (int n = 0; n < nameEmployees.size(); n++) {
+            System.out.println(nameEmployees.get(n) + " - " + salaryEmployees.get(n));
+        }
 
 
-
-
-
-
-
-
+        //System.out.println("Tha salary of the employees is : "+salaryEmployees);
 
     }
 }
