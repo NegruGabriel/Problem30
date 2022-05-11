b2932ab2d25078b2d79a0c504c167035ddfbb229
NegruGabriel <87751537+NegruGabriel@users.noreply.github.com>
2022-05-11 12:05:19 +0200

    Inceput problema

---
+++ src/Problem30/Problem30.java
@@ -0,0 +1,27 @@
+package Problem30;
+
+import java.util.ArrayList;
+
+public class Problem30 {
+    public static void main(String[] args) {
+        boolean shouldTerminateTheProgram =true;
+        boolean canContinueRunProgram =false;
+
+        ArrayList<String>nameEmployees =new ArrayList<>();
+        nameEmployees.add("Florin Neagu");
+        nameEmployees.add("Diana Albu");
+        nameEmployees.add("Elena Silvan");
+        nameEmployees.add("Mirel Condur");
+
+        ArrayList<Double>salaryEmployees = new ArrayList<>();
+        salaryEmployees.add(99999.99);
+        salaryEmployees.add(10000000.05);
+        salaryEmployees.add(987546.00);
+        salaryEmployees.add(7896.65);
+        System.out.println("Hello user. Welcome to the program");
+
+
+
+
+    }
+}
